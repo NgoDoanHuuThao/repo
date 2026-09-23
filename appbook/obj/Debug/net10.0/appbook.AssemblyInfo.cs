5d924ec910162e31941a8ee83ce9289b88fb6449
huuthaohk <huuthaohk@gmail.com>
2026-09-23 22:16:47 +0700

    Remove tracked .vs folder and update gitignore

--- appbook/obj/Debug/net10.0/appbook.AssemblyInfo.cs
+++ appbook/obj/Debug/net10.0/appbook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appbook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4b196202ad0b0b971e6fdacceb136ca815b742")]
 [assembly: System.Reflection.AssemblyProductAttribute("appbook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appbook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
